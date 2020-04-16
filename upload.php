69e3ba6904a71e95fbb8b32788b218786a9cc07d
AnionaStella <eiljon88@gmail.com>
2020-04-16 14:56:59 +0200

    added missing spaces

--- upload.php
+++ upload.php
@@ -18,44 +18,44 @@ if(isset($_POST["submit"])) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+        echo "File is not an image. ";
         $uploadOk = 0;
     }
     // Check if file already exists
     if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
+        echo "Sorry, file already exists. ";
         $uploadOk = 0;
     }
     // Check file size
     if ($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
+        echo "Sorry, your file is too large. ";
         $uploadOk = 0;
     }
     // Allow certain file formats
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed. ";
         $uploadOk = 0;
     }
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
+        echo "Sorry, your file was not uploaded. ";
     // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-          echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+          echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded. ";
           // Fetch old file name
           $animal = $animalRepository->getAnimalById($_POST["animalId"]);
           $oldImage = $animal["image"];
           // erase file
           if($oldImage){
             unlink("uploads/$oldImage");
-            echo "deleted old file"; 
+            echo "deleted old file "; 
           }
           //update image file
           $animalRepository->setImageById(basename( $_FILES["fileToUpload"]["name"]),$_POST["animalId"]); 
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            echo "Sorry, there was an error uploading your file. ";
         }
     }
 }
